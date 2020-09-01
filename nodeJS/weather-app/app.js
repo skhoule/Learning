496e95bf6932e1e27719f060f9ab1e402845d345
Sal Khoule <S.khoule@gmail.com>
2020-09-01 15:16:37 -0400

    9/1/20

--- nodeJS/weather-app/app.js
+++ nodeJS/weather-app/app.js
@@ -1,26 +1,26 @@
-const request = require('request')
+const geocode = require('./utils/geocode')
+const forecast = require('./utils/forecast')
 
-const url = 'https://api.darksky.net/forecast/e4c81fdec54d67dcd3845f444be30c59/37.8267,-122.4233'
-request({ url: url, json: true, time: true }, (error,response) => {
-    if (error) {
-        console.log('Unable to connect to weather service! \n', error)
+const locale = process.argv[ 2 ]
+console.log(locale);
 
-    } else if (response.body.error){
-        console.log('Error Code: '+ response.body.code,'\n' + response.body.error)
-    } else {
-        console.log(`${response.body.daily.data[0].summary} It is corrently ${response.body.currently.temperature} degrees out. There is a ${response.body.currently.precipProbability} chance of rain.`, '\nTime:' + response.elapsedTime + 'ms');
-    }
-})
+if (!locale) {
+    console.log('Please provide location.')
+} else {
+    geocode(locale, (geocodeError, {latitude, longtitude, location}) => {
+        if (geocodeError) {
+            // Instead of using else, Return stops execution.
+            return console.log(geocodeError);
+        }
 
-const url2 = "https://api.mapbox.com/geocoding/v5/mapbox.places/NewYorkCity.json?access_token=pk.eyJ1Ijoic2tob3VsZSIsImEiOiJjanRxZGIyOTMwZTQ1NDNtMmpnb2ZwZmFiIn0.D6hKgSM3aW51QFKRXZJadA";
-request({ url: url2, json: true, time: true }, (error, response) => {
-    if (error) {
-        console.log('Unable to connect to location service! \n', error)
-    } else if (response.body.features.length === 0){
-        console.log('Unable to find locaiton')
-    } else {
-        const latitude = response.body.features[0].center[1]
-        const longtitude = response.body.features[0].center[0]
-        console.log(latitude, longtitude, '\nTime:' + response.elapsedTime + 'ms ');
-    }
-})
+        forecast(latitude, longtitude, (forecastError, forecastData) => {
+            if (forecastError) {
+                return console.log(forecastErrorr);
+            }
+            console.log(location);
+            console.log(forecastData);
+
+        })
+    })
+
+}
