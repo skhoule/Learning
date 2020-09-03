63caa62fe885cfa5810a37ab6d2353ad96a4e4f9
Sal Khoule <S.khoule@gmail.com>
2020-09-03 15:47:04 -0400

    Webserver

--- nodeJS/web-server/public/js/app.js
+++ nodeJS/web-server/public/js/app.js
@@ -1,1 +1,35 @@
-console.log('Client side JS is loading');
+const weatherform = document.querySelector('form')
+const search = document.querySelector('input')
+
+const messageOne = document.querySelector('#message-1')
+const messageTwo = document.querySelector('#message-2')
+
+weatherform.addEventListener('submit', (e) => {
+    e.preventDefault()
+    const location = search.value
+
+    messageOne.textContent = 'Loading...'
+    messageTwo.textContent = ''
+
+
+    fetch(`http://localhost:3000/weather?address=${location}`).then((response) => {
+        response.json().then((data) => {
+            console.log(data);
+
+            if (data.error) {
+                messageOne.textContent = data.error
+                messageTwo.textContent = ''
+                return
+            }
+            messageOne.textContent = data.location;
+            messageTwo.textContent = `It's ${data.forecast.temperature}. ${data.forecast.summary}`;
+        })
+    })
+})
+
+// console.log('Message');
+// fetch('https://puzzle.mead.io/puzzle').then((response) => {
+//     response.json().then((data) => {
+//         console.log(data);
+//     })
+// })
